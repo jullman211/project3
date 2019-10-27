b7f110f0770cb972ff9d713820b5836464ccc5bf
Josh Ullman <ullmanjosh@yahoo.com>
2019-10-26 22:40:19 -0500

    Update MesoEquivalent.java
    edit

--- MesoEquivalent.java
+++ MesoEquivalent.java
@@ -64,8 +64,8 @@ public class MesoEquivalent {
 				
 				avgValueString = calCompare(compareString);
 				if(avgValueString == compareTestAvgBase) {
-					storedAverage.put(compareString, 76);
-					//sortedAverage.put(compareString,76);
+					//storedAverage.put(compareString, 76);
+					sortedAverage.put(compareString,76);
 				}
 				
 			}
